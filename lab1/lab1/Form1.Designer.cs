c1f9cfc4b9c6250fb92e9fa3d102182c558b3c57
smolakoo <83919315+smolakoo@users.noreply.github.com>
2021-05-09 19:10:08 +0200

    Add files via upload
    zadanie 1

---
+++ lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bSum = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.bIlo = new System.Windows.Forms.Button();
+            this.bExit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bSum
+            // 
+            this.bSum.Location = new System.Drawing.Point(519, 90);
+            this.bSum.Name = "bSum";
+            this.bSum.Size = new System.Drawing.Size(102, 23);
+            this.bSum.TabIndex = 0;
+            this.bSum.Text = "Suma";
+            this.bSum.UseVisualStyleBackColor = true;
+            this.bSum.Click += new System.EventHandler(this.bSum_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(317, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(317, 291);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(129, 23);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "z";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(317, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(317, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 23);
+            this.textBox3.TabIndex = 8;
+            // 
+            // bIlo
+            // 
+            this.bIlo.Location = new System.Drawing.Point(519, 131);
+            this.bIlo.Name = "bIlo";
+            this.bIlo.Size = new System.Drawing.Size(102, 23);
+            this.bIlo.TabIndex = 9;
+            this.bIlo.Text = "lloczyn";
+            this.bIlo.UseVisualStyleBackColor = true;
+            this.bIlo.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // bExit
+            // 
+            this.bExit.Location = new System.Drawing.Point(519, 175);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(102, 23);
+            this.bExit.TabIndex = 10;
+            this.bExit.Text = "Wyjście";
+            this.bExit.UseVisualStyleBackColor = true;
+            this.bExit.Click += new System.EventHandler(this.bExit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(212, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "wynik";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.bExit);
+            this.Controls.Add(this.bIlo);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bSum);
+            this.Name = "Form1";
+            this.Text = "Kwadrat liczby";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bSum;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button bIlo;
+        private System.Windows.Forms.Button bExit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ lab1/lab1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bSum_Click(object sender, EventArgs e)
+        {
+            try 
+            
+            {
+                float x = float.Parse(textBox1.Text);
+                float y = float.Parse(textBox2.Text);
+                float z = float.Parse(textBox3.Text);
+                textBox4.Text = (x + y + z).ToString();
+            
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Podano błędne dane", "Uwaga!", MessageBoxButtons.OK);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(textBox1.Text);
+                float y = float.Parse(textBox2.Text);
+                float z = float.Parse(textBox3.Text);
+                textBox4.Text = (x * y * z).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Podano błędne dane", "Uwaga!", MessageBoxButtons.OK);
+            }
+        }
+
+        private void bExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
